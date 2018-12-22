30cf94f1ccc3790548102a3e65500bfbfd70e3a8
Naveen Singh <naveen@physiz.com>
2018-12-22 17:12:32 +0530

    shell script modified

--- phycore.sh
+++ phycore.sh
@@ -16,6 +16,15 @@ echo "Install dependency"
 
 npm install
 
+
+
 echo "Restart server"
 echo "====================================="
 NODE_ENV=dev pm2 start /home/ubuntu/phycore/server/server.js --name phycore
+
+
+RESULT=$(mocha modules/automation/tests | grep -E 'passing|failing')
+
+echo $RESULT
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#ci-server\", \"username\": \"ci-bot\", \"text\": \"Code pushed to phycore/develop \n '$RESULT' \", \"icon_emoji\": \":robot_face:\"}" https://hooks.slack.com/services/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
