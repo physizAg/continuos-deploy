37f3daa09b4e6d783db636f6c34d096aae818332
Naveen Singh <naveen@physiz.com>
2018-12-26 14:44:28 +0530

    updated shell script

--- phycore.sh
+++ phycore.sh
@@ -23,8 +23,16 @@ echo "====================================="
 NODE_ENV=dev pm2 start /home/ubuntu/phycore/server/server.js --name phycore
 
 
-RESULT=$(mocha modules/automation/tests | grep -E 'passing|failing')
+PASSING=$(mocha modules/automation/tests | grep -E 'passing')
+FAILING=$(mocha modules/automation/tests | grep -E 'failing')
 
-echo $RESULT
+echo $PASSING
+echo $FAILING
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#ci-server\", \"username\": \"ci-bot\", \"text\": \"Code pushed to phycore/develop \n '$RESULT' \", \"icon_emoji\": \":robot_face:\"}" https://hooks.slack.com/services/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
+
+curl -X POST --data-urlencode "payload={
+	\"channel\": \"#ci-server\", 
+	\"username\": \"ci-bot\", 
+	\"text\": \"Code pushed to phycore/develop \n :white_check_mark: $PASSING \n :x: $FAILING \",
+	 \"icon_emoji\": \":robot_face:\"
+	}" https://hooks.slack.com/services/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
